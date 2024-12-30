7bf9a6521d4094f2a2ca916e7ef31403b2035cb1
FlashScript <itzflashscript@gmail.com>
2024-12-30 19:36:49 +0330

    React 19 in Server Component

---
+++ src/components/ServerComponent.tsx
@@ -0,0 +1,13 @@
+// Root 👉🏻 "src" Folder 👉🏻 "components" Folder 👉🏻 ServerComponent.tsx
+
+const ServerComponent = () => {
+
+    console.log("ServerComponent");
+
+    return (
+        <h1>Server Component</h1>
+    )
+
+}
+
+export default ServerComponent
