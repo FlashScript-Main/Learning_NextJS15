a239ffaa79c5fc538073dd3184ad315c81d8e64e
FlashScript <itzflashscript@gmail.com>
2024-12-31 20:36:31 +0330

    Server Components VS Client Components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
 // Root 👉🏻 "src" Folder 👉🏻 "app" Folder 👉🏻 page.tsx
 
-import ClientComponent from "@/components/ClientComponent"
-import ServerComponent from "@/components/ServerComponent"
 
 const Home = () => {
 
     return (
         <div>
-            <ServerComponent />
-            <ClientComponent />
+            Home
         </div>
     )
 
